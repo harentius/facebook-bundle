aabec0bc4a45e224137ddbb79552b4dd26580b3f
Yaroslav Osypenko <harentius@yandex.ru>
2016-08-26 20:27:13 +0300

    fix for php 5.4.*

--- src/DependencyInjection/HarentiusFacebookExtension.php
+++ src/DependencyInjection/HarentiusFacebookExtension.php
@@ -2,7 +2,6 @@
 
 namespace Harentius\FacebookBundle\DependencyInjection;
 
-use Facebook\Facebook;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -17,7 +16,7 @@ class HarentiusFacebookExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $definition = new Definition(Facebook::class, [
+        $definition = new Definition('Facebook\Facebook', [
             [
                 'app_id' => $config['app_id'],
                 'app_secret' => $config['app_secret'],
